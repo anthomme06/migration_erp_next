7a5fda60c355dee609a8bac2b3f5fde7f48fc986
Josmel Diaz <josmeldiaz21@gmail.com>
2021-04-21 15:40:20 -0400

    busqueda de proveedores por nombre

--- migracion.py
+++ migracion.py
@@ -10,6 +10,7 @@ headers = {
 url_base = "https://contabilidad.coopbarcelona.com/api/resource/"
 url_productos = url_base + "Item"
 url_proveedor = url_base + "Supplier"
+url_pi = url_base + "Purchase Invoice"
 
 col_productos = [
     "item_group",
@@ -29,10 +30,25 @@ col_proveedores = [
     "supplier_group",
     "country"
 ]
-
+col_pi = [
+    "docstatus",
+    "naming_series",
+    "supplier",
+    "tax_id",
+    "posting_date",
+    "due_date",
+    "credit_to",
+    "bill_no",
+    "item_code",
+    "qty",
+    "rate",
+    "expense_account"
+]
 
 productos = pd.read_excel('productos.xlsx', usecols=col_productos).fillna("")
-proveedor = pd.read_excel('proveedores.xlsx', usecols=col_proveedores).fillna("")
+proveedor = pd.read_excel(
+    'proveedores.xlsx', usecols=col_proveedores).fillna("")
+pi = pd.read_excel('facturas_de_compra_test2.xlsx', usecols=col_pi).fillna("")
 
 # [print(proveedor[columna][fila]) for fila in proveedor.index for columna in proveedor.columns]
 
@@ -62,6 +78,7 @@ def create_item(df, url):
 
 def create_supplier(df, url):
     for row in df.iterrows():
+        row[1]['tax_id'] = row[1]['tax_id'].replace("-", "")
         item = {
             "tax_id": row[1]['tax_id'],
             "id_type": row[1]['id_type'],
@@ -92,6 +109,7 @@ def delete_records(df, url, column_name):
 
 
 def find_supplier(url, rnc):
+    rnc = rnc.replace('-', '')
     url = url + "?filters=[[\"Supplier\",\"tax_id\",\"=\",\"" + rnc + "\"]]"
     respuesta = requests.request("GET", url, headers=headers)
     try:
@@ -101,61 +119,42 @@ def find_supplier(url, rnc):
         return False
 
 
-def create_purchase_invoices(df, url):
-    for row in df.iterrows():
-        item = {
-            "tax_id": row[1]['tax_id'],
-            "id_type": row[1]['id_type'],
-            "supplier_name": row[1]['supplier_name'],
-            "supplier_type": row[1]['supplier_type'],
-            "supplier_group": row[1]['supplier_group'],
-            "country": row[1]['country']
-        }
-        item = {
-            "docstatus": 1,
-            "naming_series": "ACC-PINV-.YYYY.-",
-            "supplier": row[1]['tax_id'],
-            "tax_id": row[1]['tax_id'],
-            "bill_no": row[1]['tax_id'],
-            "posting_date": row[1]['tax_id'],
-            "due_date": row[1]['tax_id'],
-            "items": [
-                {
-                    "item_code": "COMBUSTIBLE",
-                    "qty": 1,
-                    "rate": 7500,
-                    "expense_account": "620703 - AGUA - CB"
-                }
-            ],
-            "credit_to": row[1]['tax_id']
-        }
-        if requests.request("POST", url, data=json.dumps(item), headers=headers):
-            print("Proveedor " + row[1]['supplier_name'] + " creado")
-        else:
-            print("Proveedor: " + row[1]
-                  ['supplier_name'] + " no pudo ser creado")
+# def create_purchase_invoices(df, url):
+#     for row in df.iterrows():
+#         items = [{
+#             "item_code": row[1]['item_code'],
+#             "qty": row[1]['qty'],
+#             "rate": row[1]['rate'],
+#             "expense_account": row[1]['expense_account']
+#         }]
+#         while not row[1]['item_code']:
 
-    # {
-    #     "docstatus": 1,
-    #     "naming_series": "ACC-PINV-.YYYY.-",
-    #     "supplier": "SUNIX PETROLEUM SRL",
-    #     "tax_id": "130192731",
-    #     "posting_date": "2021-04-16",
-    #     "due_date": "2021-04-16",
-    #     "items": [
-    #         {
-    #             "item_code": "COMBUSTIBLE",
-    #             "qty": 1,
-    #             "rate": 7500,
-    #             "expense_account": "620703 - AGUA - CB"
-    #         }
-    #     ],
-    #     "credit_to": "210201 - CUENTA POR PAGAR SUPLIDORES - CB"
-    # }
+#             items = items.append
+#             items = {
+#                 "item_code": row[1]['item_code'],
+#                 "qty": row[1]['qty'],
+#                 "rate": row[1]['rate'],
+#                 "expense_account": row[1]['expense_account']
+#             }
+#         if requests.request("POST", url, data=json.dumps(item), headers=headers):
+#             print("Factura de Compra: " + str(row[1]['tax_id']) + " creado")
+#         else:
+#             print("Factura de Compra: " +
+#                   str(row[1]['tax_id']) + " no pudo ser creado*******")
 
+def find_supplier_by_name(url, name):
+    url = url + "?filters=[[\"Supplier\",\"name\",\"=\",\"" + name + "\"]]&fields=[\"name\",\"tax_id\"]"
+    respuesta = requests.request("GET", url, headers=headers)
+    try:
+        return respuesta.json()['data'][0]['tax_id']
+        # respuesta.json()['data'][0]['name']
+        return True
+    except:
+        return False
 
 # create_item(productos, url_productos)
 # create_supplier(proveedor, url_proveedor)
 # delete_records(proveedor,url_proveedor,"supplier_name")
-print(find_supplier(url_proveedor,"131191152"))
-
+# print(find_supplier(url_proveedor,"130563748"))
+# create_purchase_invoices(pi, url_pi)
+print(find_supplier_by_name(url_proveedor,"SEGUROS UNIVERSAL"))
